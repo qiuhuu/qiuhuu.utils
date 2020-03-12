9262b194bbf15995ab549700119504a9ba8eff31
qiuhuu <1094510079@qq.com>
2020-03-12 14:12:35 +0800

    '添加二维码生成工具'

---
+++ src/main/java/com/qiuhuu/utils/qrcode/QRCodeMain.java
@@ -0,0 +1,20 @@
+package com.qiuhuu.utils.qrcode;
+
+import com.qiuhuu.utils.qrcode.utils.QRCodeUtils;
+
+/**
+ * @Description:
+ * @Author: qiuhuu
+ * @Create: 2020-03-03 10:52
+ */
+public class QRCodeMain {
+    public static void main(String[] args) throws Exception {
+        // 存放在二维码中的内容
+        String text = "csa";
+        // 嵌入二维码的图片路径
+        String imgPath = "E://Download/18107.jpg";
+        // 生成的二维码的路径及名称
+        String destPath = "E://";
+        QRCodeUtils.encode(text,destPath);
+    }
+}
